3cf3686e99f9ab07c5d664854f6039342082c0a2
Chase P. Miller <cpmills26@gmail.com>
2021-04-03 16:09:46 -0400

    Organization of Files

--- src/test/NoiseTester.java
+++ src/test/NoiseTester.java
@@ -1,16 +1,17 @@
 package test;
 
-import main.*;
-import main.Random.Generator;
-import java.awt.image.*;
+import main.generators.*;
+import main.noise.*;
+import main.util.*;
+import main.noise.Random.Generator;
+
 import java.io.*;
-import javax.imageio.*;
 
 public class NoiseTester {
     public static void main(String[] args) throws IOException {
-        // NoiseMapGenerator.generate(new White(450193589), 512, 512);
-        // TerrainGenerator.generate("WhiteNoise.png", 512, 256, 7, 89);
-        // testRandom(450193589, Generator.Canon);
+        NoiseMapGenerator.generate(new White(450193589), 512, 512);
+        TerrainGenerator.generate("WhiteNoise.png", 512, 256, 7, 89);
+        testRandom(450193589, Generator.Canon);
     }
 
     private static void testRandom(long seed, Generator generator) {
